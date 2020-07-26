1959f44841c033765913500116984113894a0fde
Minegeeks <17189670+brandonkp@users.noreply.github.com>
2020-07-26 00:42:07 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+var api = "http://xmr.minegeeks.com:8117";
+var email = "djzecone.bp@gmail.com";
+var telegram = "https://t.me/YourPool";
+var discord = "https://discordapp.com/invite/YourPool";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
+
+
